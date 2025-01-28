kasper93/mpv

commit af384df21982640f02395143d16e29d87b2122fa

vo_kitty: unlink shared memory on vo close

video/out/vo_kitty.c
@@ -114,6 +114,7 @@ static void close_shm(struct priv *p)
         p->buffer = NULL;
     }
     if (p->shm_fd != -1) {
+        shm_unlink(p->shm_path);
         close(p->shm_fd);
         p->shm_fd = -1;
     }

commit 87d947a26d2dba8b644a0c7e594f80c25a23474d

vo_kitty: unlink shared memory only on vo close
We don't want to unlink on flip_page().

As a bonus fix leaked mpi image.

Fixes: af384df21982640f02395143d16e29d87b2122fa

video/out/vo_kitty.c
@@ -114,7 +114,6 @@ static void close_shm(struct priv *p)
         p->buffer = NULL;
     }
     if (p->shm_fd != -1) {
-        shm_unlink(p->shm_path);
         close(p->shm_fd);
         p->shm_fd = -1;
     }
@@ -130,6 +129,7 @@ static void free_bufs(struct vo* vo)
 
     if (p->opts.use_shm) {
         close_shm(p);
+        shm_unlink(p->shm_path);
     } else {
         talloc_free(p->buffer);
     }
@@ -282,9 +282,8 @@ static bool draw_frame(struct vo *vo, struct vo_frame *frame)
     if (!p->opts.use_shm)
         av_base64_encode(p->output, p->output_size, p->buffer, p->buffer_size);
 
-    talloc_free(mpi);
-
 done:
+    talloc_free(mpi);
     return VO_TRUE;
 }
 